d491ff0235ba9f2de4775bdde40ab839878a12a9
Shreyansh36m <2303031560039@paruluniversity.ac.in>
2025-11-29 00:24:50 +0530

    HelloWorld..
    My unique way of showing the how a beginner should be taught first program

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+package Basics;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World"); // Line Spacing -> ln
+        System.out.print("I Love to code");
+        System.out.print("\nMy key Learning from coding is Consistency");// Alternative way
+        System.out.println("I am sure my projects will impress you !");
+        System.out.println("Good Luck!");
+    }
+}
