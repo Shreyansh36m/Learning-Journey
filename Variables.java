f121c3f50bff90e5a6f5ba27217207fa266ab3bb
Shreyansh36m <2303031560039@paruluniversity.ac.in>
2025-11-29 18:10:15 +0530

    Add files via upload

---
+++ Variables.java
@@ -0,0 +1,32 @@
+package Basics;
+
+public class Variables {
+    public static void main(String[] args) {
+//        int x ; // Declaration
+//        x = 5; // Initialization
+//        System.out.println(x); // Printing the value of x
+//
+//        int y = 3; // Declaration and Initialization in same line
+//        System.out.println(y); // Printing the value of y
+
+//        int a = 10;
+//        int b = 5;
+//        int z = a + b; // Adding and storing them in z
+//        System.out.println(z);
+//        System.out.println(a+b); // Also true
+
+//        // All arithmetic operations
+//        int x = a+b;
+//        int y = a-b;
+//        int t = a*b;
+//        int j = a/b;
+//        int k = a%b; // Gives remainder, in this case zero
+//        System.out.println(x);
+//        System.out.println(y);
+//        System.out.println(t);
+//        System.out.println(j);
+//        System.out.println(k);
+
+
+    }
+}
